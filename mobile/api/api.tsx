03285c5c35001cc5ca83b08044f68868cc64e58a
Bryan Andriamasy <bryan.andriamasy@epita.fr>
2020-12-15 13:20:46 +0100

    [mobile][api] new routes added but no tested

--- mobile/api/api.tsx
+++ mobile/api/api.tsx
@@ -71,8 +71,126 @@ const postCreateUser = async (
     }
 }
 
+const getPeople = async () => { //FIXME
+    try {
+      let response = await fetch(`${baseUrl.API_URL}/people`);
+      let json = await response.json();
+      console.log(JSON.stringify(json));
+      Actions.refresh();
+    } catch (error) {
+      console.error(error);
+    }
+};
+
+const postIncrementHandwashing = async (id: number) => { //FIXME
+    try {
+        let response = await fetch(`${baseUrl.API_URL}/hand-washing`, {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                id: id}),
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const getStats = async () => { //FIXME
+    try {
+      let response = await fetch(`${baseUrl.API_URL}/statistics-user`);
+      let json = await response.json();
+      console.log("stats : countHandwashing : " + JSON.stringify(json.countHandwashingDay) + " countHandwashingMonth : " + JSON.stringify(json.countHandwashingDay));
+      Actions.refresh({countHandwashingDay:json.countHandwashingDay, countHandwashingMonth: json.countHandwashingMonth});
+    } catch (error) {
+      console.error(error);
+    }
+};
+
+const postProductRefill = async (id: number) => { //FIXME
+    try {
+        let response = await fetch(`${baseUrl.API_URL}/product/refill`, {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                id: id}),
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const postProducts = async (idEntreprise: number) => { //FIXME
+    try {
+        let response = await fetch(`${baseUrl.API_URL}/products`, {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                idEntreprise: idEntreprise}),
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const getEntreprise = async () => { //FIXME
+    try {
+      let response = await fetch(`${baseUrl.API_URL}/entreprise`);
+      let json = await response.json();
+
+      console.log("entreprise : " + JSON.stringify(json.entreprise));
+      Actions.refresh({entreprise:json.entreprise});
+    } catch (error) {
+      console.error(error);
+    }
+};
+
+const getAllEntreprises = async () => { //FIXME
+    try {
+      let response = await fetch(`${baseUrl.API_URL}/all-entreprise`);
+      let json = await response.json();
+
+      //Actions.refresh();
+    } catch (error) {
+      console.error(error);
+    }
+};
+
+const postEntreprise = async (name: string, siret: string) => { //FIXME
+    try {
+        let response = await fetch(`${baseUrl.API_URL}/entreprise`, {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: name,
+                siret: siret}),
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 export default {
     getHello,
     postUserAuth,
-    postCreateUser
+    postCreateUser,
+    postIncrementHandwashing,
+    getStats,
+    getPeople,
+    postProductRefill,
+    postProducts,
+    getEntreprise,
+    getAllEntreprises,
+    postEntreprise
 };
